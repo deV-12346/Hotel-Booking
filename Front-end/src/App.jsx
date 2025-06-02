efde7f66b8d2c1b5478b4a63269485faf6e7d3ef
Dev Raj <dr395108@gmail.com>
2025-06-02 18:33:07 +0530

    Making a project on hotel boking

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import {Route, Routes, useLocation} from "react-router-dom"
+import Home from './Pages/Home'
+const App = () => {
+  const isOwner = useLocation().pathname.includes("owner")
+  return (
+    <div>
+     {!isOwner && <Navbar/>} 
+    <div className='min-h[70vh]'>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+     </Routes>
+    </div>
+    </div>
+  )
+}
+
+export default App
