c1e1934a10847bfca6d341f0ac927dec4b5891b2
Dev Raj <dr395108@gmail.com>
2025-06-11 13:05:04 +0530

    Added add room form in hotel owner panel

--- Front-end/src/Pages/HotelOwner/AddRoom.jsx
+++ Front-end/src/Pages/HotelOwner/AddRoom.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Title from "../../Components/Titlte"
+import { assets } from '../../assets/assets'
 const AddRoom = () => {
+
+   const [images,setimages] = useState({
+       1:null,
+       2:null,
+       3:null,
+       4:null
+   })
+  const [inputs,setInputs] = useState({
+       roomType:"",
+       pricePerNight:0,
+       amenitites:{
+         'Free wifi' : false,
+         'Free Breakfast' : false,
+         'Room Service' : false,
+         'Mountain view' : false,
+         'Pool access': false
+       }
+  })
+
+
   return (
-    <div>AddRoom</div>
+    <form action="">
+      <Title align="left" font="outfit" 
+      title="Add Room" subtitle="Fill in the details carefully and accurate room deatils 
+      pricing ,amenitities ,to ehnace the user booking exprience " />
+      {/* images area */}
+      <p className='text-gray-800 mt-10'>Images</p>
+      <div className='grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap '>
+      {Object.keys(images).map((key)=>(
+          <label id={`roomImage${key}`} key={key}>
+             <img src={images[key] ? URL.createObjectURL(images[key]) : assets.uploadArea } 
+             alt="image" className='max-h-13 cursor-pointer opcity-80' />
+             <input type="file"  accept='image/*' key={`roomImage${key}`}
+             onChange={e=>setimages({...images,[key] : e.target.files[0]})} hidden/>
+          </label>
+      ))}
+      </div>
+      <div className='w-full flex max-sm:flex-col sm:gap-4 mt-4 '>
+        <div className='flex-1 max-w-48'>
+          <p className='text-gray-800 mt-4'>Room Type</p>
+          <select value={inputs.roomType}
+          onChange={e=>setInputs({...inputs,roomType:e.target.value})} className='border border-gray-300 opacity-70 
+          mt-1 rounded p-2 w-full'>
+            <option value="">Select Room Type</option>
+            <option value="Single Bed">Single Bed</option>
+            <option value="Double Bed">Double Bed</option>
+            <option value="Luxury Bed">Luxury Bed</option>
+            <option value="Family Suite">Family Suite</option>
+          </select>
+        </div>
+
+        <div>
+        <p className='text-gray-800 mt-4'>Price
+          <span className='text-sm'>/night</span>
+        </p>
+        <input type="number" placeholder='0' className='border border-gray-300 mt-1 rounded p-2 w-24'
+        value={inputs.pricePerNight} onChange={e=>setInputs({...inputs,pricePerNight:e.target.value})} />
+      </div>
+
+      </div>
+      <p className='text-gray-800 mt-4'>Amenitites</p>
+      <div className='flex flex-col flex-wrap mt-1 text-gray-400 max-w-sm'>
+        {Object.keys(inputs.amenitites).map((amenity , index)=>(
+          <div key={index}>
+              <input type="checkbox"  id={`amenitites${index+1}`}
+              checked={inputs.amenitites[amenity]} onChange={()=>setInputs({...inputs,
+                amenitites:{...inputs.amenitites,[amenity]:!inputs.amenitites[amenity]}
+              })} />
+              <label htmlFor={`amenitites${index+1}`}> {amenity}</label>
+          </div>
+        ))}
+      </div>
+      <button className='bg-blue-600 text-white px-8 py-2 rounded mt-3 cursor-pointer'>
+        Add Room
+      </button>
+    </form>
   )
 }
 
